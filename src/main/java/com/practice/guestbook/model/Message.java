1e2f647b4ad1fdc9e914f741ebd576e60a503d3b
Chris <corday@gmail.com>
2018-01-20 16:12:03 +0000

    Main method complete, model lass finished.

---
+++ src/main/java/com/practice/guestbook/model/Message.java
@@ -0,0 +1,58 @@
+package com.practice.guestbook.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Size(max = 30)
+    private String posterName;
+
+    private Date postDate;
+
+    @Size(max = 180)
+    private String message;
+
+    public Message() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPosterName() {
+        return posterName;
+    }
+
+    public void setPosterName(String posterName) {
+        this.posterName = posterName;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
